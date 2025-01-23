64ab48c5e76ee79b03beffaa6023c9b57c985f9b
Vinra <pandiavinra@gmail.com>
2025-01-23 18:27:38 +0700

    first commit

---
+++ src/main/java/com/vinra/ids/vinra/VinraApplication.java
@@ -0,0 +1,13 @@
+package com.vinra.ids.vinra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinra/ids/vinra/VinraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinra.ids.vinra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
